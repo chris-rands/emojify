75d018a6a6d1aa9694e204aa8cb168464cc6cfba
chris-rands <c_rands100@hotmail.com>
2017-03-09 11:20:32 +0100

    Update emojify_python_script.py

--- emojify_python_script.py
+++ emojify_python_script.py
@@ -23,8 +23,7 @@ hello world
 4 + 4 = 8
 
 Disclaimers:
-Not tested on complex scripts, so don't rely on this script to work, it is probably very easy to break.
-The script will not always produce an identical output for the same input; replace the dictionaries with collections.OrderedDict to achieve that.
+Not tested on complex scripts, so don't rely on this script to work, not guarnetted to work at all, and it is probably easy to break.
 
 Licence:
 Copyright (c) 2017, Chris Rands
