f485e4c9f28ed63a672f737ca4a0321dc6bbf1e9
Chris Rands <c_rands100@hotmail.com>
2018-04-11 15:30:38 +0200

    improve line length of comment

--- emojify_python_script.py
+++ emojify_python_script.py
@@ -15,8 +15,9 @@ python emojify_python_script.py --input input_script.py --output output_script.p
 Not tested on complex scripts, so don't rely on this script to work,
 not guaranteed to work at all, and it is probably easy to break.
 One case it will fail is with non-unicode characters.
-Also note if you want to securely protect your code
-from the eyes of others, this is NOT a good way, so find another way.
+
+Also note if you want to securely protect your code from the eyes of others,
+this is NOT a good way, so find another way.
 
 # License
 Copyright (c) 2018, Chris Rands.
