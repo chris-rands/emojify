e057168b3ea261b41276f5119e45be40a3291d01
Chris Rands <c_rands100@hotmail.com>
2019-10-23 09:54:16 +0200

    blackify

--- emojify.py
+++ emojify.py
@@ -1,44 +1,50 @@
 #!/usr/bin/env python
 
-'''
+"""
 emojify.py
 
 Obfuscate your python script (or indeed any plain text file) by converting an 
 input script to an output script that functions the same (hopefully) but 
 encodes the code as emoji icons, currently emoticons.
-'''
+"""
 
 import argparse
 from collections import OrderedDict
 from pprint import pformat
 
-__author__ = 'Chris Rands'
-__copyright__ = 'Copyright (c) 2017-2019, Chris Rands'
+__author__ = "Chris Rands"
+__copyright__ = "Copyright (c) 2017-2019, Chris Rands"
 
 try:
     range = xrange
 except NameError:
     pass  # Python 3
 
-EMOTICONS = [':)', ':D', ':P', ':S', ':(', '=)', '=/', ':/', ':{', ';)']
+EMOTICONS = [":)", ":D", ":P", ":S", ":(", "=)", "=/", ":/", ":{", ";)"]
 # TODO: Add other alphabets as options including real emojis
 MAX_STR_LEN = 70
 
 
 def run_argparse():
     """User arguments"""
-    parser = argparse.ArgumentParser(description='''
+    parser = argparse.ArgumentParser(
+        description="""
     Obfuscate your python script by converting an input script to an output script
     that functions the same (hopefully) but encodes the code as emoji icons, currently emoticons.
-    -- Chris Rands, 2017-2019''')
-    parser.add_argument('-i', '--input', required=True, help='input python script name')
-    parser.add_argument('-o', '--output', required=True, help='output python script name')
+    -- Chris Rands, 2017-2019"""
+    )
+    parser.add_argument("-i", "--input", required=True, help="input python script name")
+    parser.add_argument(
+        "-o", "--output", required=True, help="output python script name"
+    )
     return parser.parse_args()
 
 
 def chunk_string(in_s, n):
     """Chunk string to max length of n"""
-    return '\n'.join('{}\\'.format(in_s[i:i+n]) for i in range(0, len(in_s), n)).rstrip('\\')
+    return "\n".join(
+        "{}\\".format(in_s[i : i + n]) for i in range(0, len(in_s), n)
+    ).rstrip("\\")
 
 
 def encode_string(in_s, alphabet):
@@ -47,19 +53,26 @@ def encode_string(in_s, alphabet):
     # Note Python 2 and 3 inputs differ slightly due to pformat()
     d1 = OrderedDict(enumerate(alphabet))
     d2 = OrderedDict((v, k) for k, v in d1.items())
-    return ('from collections import OrderedDict\n'
-            'exec("".join(map(chr,[int("".join(str({}[i]) for i in x.split())) for x in\n'
-            '"{}"\n.split("  ")])))\n'.format(pformat(d2), chunk_string('  '.join(
-            ' '.join(d1[int(i)] for i in str(ord(c))) for c in in_s), MAX_STR_LEN)))
+    return (
+        "from collections import OrderedDict\n"
+        'exec("".join(map(chr,[int("".join(str({}[i]) for i in x.split())) for x in\n'
+        '"{}"\n.split("  ")])))\n'.format(
+            pformat(d2),
+            chunk_string(
+                "  ".join(" ".join(d1[int(i)] for i in str(ord(c))) for c in in_s),
+                MAX_STR_LEN,
+            ),
+        )
+    )
 
 
 def main(in_file, out_file):
     """Read input and write output file"""
-    with open(in_file) as in_f, open(out_file, 'w') as out_f:
+    with open(in_file) as in_f, open(out_file, "w") as out_f:
         # This assumes it's ok to read the entire input file into memory
         out_f.write(encode_string(in_f.read(), EMOTICONS))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = run_argparse()
     main(args.input, args.output)
--- test_emojify.py
+++ test_emojify.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
 
-'''
+"""
 test_emojify.py
 
 Test emojify.py for basic functionality
-'''
+"""
 
 import sys
 import unittest
 from emojify import encode_string, EMOTICONS
 
-__author__ = 'Chris Rands'
-__copyright__ = 'Copyright (c) 2019, Chris Rands'
+__author__ = "Chris Rands"
+__copyright__ = "Copyright (c) 2019, Chris Rands"
 
 INPUT1 = "print('hello world')\n"
-OUTPUT1 = 'from collections import OrderedDict\nexec("".join(map(chr,[int("".join(str(OrderedDict([(\':)\', 0),\n             (\':D\', 1),\n             (\':P\', 2),\n             (\':S\', 3),\n             (\':(\', 4),\n             (\'=)\', 5),\n             (\'=/\', 6),\n             (\':/\', 7),\n             (\':{\', 8),\n             (\';)\', 9)])[i]) for i in x.split())) for x in\n":D :D :P  :D :D :(  :D :) =)  :D :D :)  :D :D =/  :( :)  :S ;)  :D :) \\\n:(  :D :) :D  :D :) :{  :D :) :{  :D :D :D  :S :P  :D :D ;)  :D :D :D \\\n :D :D :(  :D :) :{  :D :) :)  :S ;)  :( :D  :D :)"\n.split("  ")])))\n'
+OUTPUT1 = "from collections import OrderedDict\nexec(\"\".join(map(chr,[int(\"\".join(str(OrderedDict([(':)', 0),\n             (':D', 1),\n             (':P', 2),\n             (':S', 3),\n             (':(', 4),\n             ('=)', 5),\n             ('=/', 6),\n             (':/', 7),\n             (':{', 8),\n             (';)', 9)])[i]) for i in x.split())) for x in\n\":D :D :P  :D :D :(  :D :) =)  :D :D :)  :D :D =/  :( :)  :S ;)  :D :) \\\n:(  :D :) :D  :D :) :{  :D :) :{  :D :D :D  :S :P  :D :D ;)  :D :D :D \\\n :D :D :(  :D :) :{  :D :) :)  :S ;)  :( :D  :D :)\"\n.split(\"  \")])))\n"
 
 INPUT2 = "print('hello world')\ndef add(n1,n2):\n    return n1 + n2\nprint('4 + 4 = {}'.format(add(4,4)))\n"
-OUTPUT2 = 'from collections import OrderedDict\nexec("".join(map(chr,[int("".join(str(OrderedDict([(\':)\', 0),\n             (\':D\', 1),\n             (\':P\', 2),\n             (\':S\', 3),\n             (\':(\', 4),\n             (\'=)\', 5),\n             (\'=/\', 6),\n             (\':/\', 7),\n             (\':{\', 8),\n             (\';)\', 9)])[i]) for i in x.split())) for x in\n":D :D :P  :D :D :(  :D :) =)  :D :D :)  :D :D =/  :( :)  :S ;)  :D :) \\\n:(  :D :) :D  :D :) :{  :D :) :{  :D :D :D  :S :P  :D :D ;)  :D :D :D \\\n :D :D :(  :D :) :{  :D :) :)  :S ;)  :( :D  :D :)  :D :) :)  :D :) :D\\\n  :D :) :P  :S :P  ;) :/  :D :) :)  :D :) :)  :( :)  :D :D :)  :( ;)  \\\n:( :(  :D :D :)  =) :)  :( :D  =) :{  :D :)  :S :P  :S :P  :S :P  :S :\\\nP  :D :D :(  :D :) :D  :D :D =/  :D :D :/  :D :D :(  :D :D :)  :S :P  \\\n:D :D :)  :( ;)  :S :P  :( :S  :S :P  :D :D :)  =) :)  :D :)  :D :D :P\\\n  :D :D :(  :D :) =)  :D :D :)  :D :D =/  :( :)  :S ;)  =) :P  :S :P  \\\n:( :S  :S :P  =) :P  :S :P  =/ :D  :S :P  :D :P :S  :D :P =)  :S ;)  :\\\n( =/  :D :) :P  :D :D :D  :D :D :(  :D :) ;)  ;) :/  :D :D =/  :( :)  \\\n;) :/  :D :) :)  :D :) :)  :( :)  =) :P  :( :(  =) :P  :( :D  :( :D  :\\\n( :D  :D :)"\n.split("  ")])))\n'
+OUTPUT2 = "from collections import OrderedDict\nexec(\"\".join(map(chr,[int(\"\".join(str(OrderedDict([(':)', 0),\n             (':D', 1),\n             (':P', 2),\n             (':S', 3),\n             (':(', 4),\n             ('=)', 5),\n             ('=/', 6),\n             (':/', 7),\n             (':{', 8),\n             (';)', 9)])[i]) for i in x.split())) for x in\n\":D :D :P  :D :D :(  :D :) =)  :D :D :)  :D :D =/  :( :)  :S ;)  :D :) \\\n:(  :D :) :D  :D :) :{  :D :) :{  :D :D :D  :S :P  :D :D ;)  :D :D :D \\\n :D :D :(  :D :) :{  :D :) :)  :S ;)  :( :D  :D :)  :D :) :)  :D :) :D\\\n  :D :) :P  :S :P  ;) :/  :D :) :)  :D :) :)  :( :)  :D :D :)  :( ;)  \\\n:( :(  :D :D :)  =) :)  :( :D  =) :{  :D :)  :S :P  :S :P  :S :P  :S :\\\nP  :D :D :(  :D :) :D  :D :D =/  :D :D :/  :D :D :(  :D :D :)  :S :P  \\\n:D :D :)  :( ;)  :S :P  :( :S  :S :P  :D :D :)  =) :)  :D :)  :D :D :P\\\n  :D :D :(  :D :) =)  :D :D :)  :D :D =/  :( :)  :S ;)  =) :P  :S :P  \\\n:( :S  :S :P  =) :P  :S :P  =/ :D  :S :P  :D :P :S  :D :P =)  :S ;)  :\\\n( =/  :D :) :P  :D :D :D  :D :D :(  :D :) ;)  ;) :/  :D :D =/  :( :)  \\\n;) :/  :D :) :)  :D :) :)  :( :)  =) :P  :( :(  =) :P  :( :D  :( :D  :\\\n( :D  :D :)\"\n.split(\"  \")])))\n"
 
 
 class TestFunctions(unittest.TestCase):
     """Unit tests"""
+
     # TODO: expand testing
 
     def test_encode_string1(self):
@@ -36,10 +37,10 @@ class TestFunctions(unittest.TestCase):
 def main():
     """Run unit tests"""
     if sys.version_info.major != 3:
-        print('Error: unit tests for Python 3 only, exiting')
+        print("Error: unit tests for Python 3 only, exiting")
         raise SystemExit
     unittest.main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
